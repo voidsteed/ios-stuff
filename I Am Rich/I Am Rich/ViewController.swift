ba60a136e7765ba33c78f2506cea0c05c6835336
Yujun Liu <dreamzack@gmail.com>
2019-05-27 23:08:17 -0700

    adding some app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yujun Liu on 5/5/19.
+//  Copyright © 2019 Yujun Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
